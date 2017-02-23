351f9c5c7a31b78642cda3109b6ddf155288fc50
hang <hang@shou.kr>
2017-02-23 16:35:26 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="APP">
+        <Header />
+        <TeslaBattery>
+        </TeslaBattery>
+      </div>
+    );
+  }
+}
+
+export default App;
